7615b5aa627b305f8cc4fa320fc59361ea428e13
srg <skabassjob@gmail.com>
2024-11-14 17:10:45 +0400

    refactor:

--- src/js/screen.js
+++ src/js/screen.js
@@ -2,49 +2,20 @@
  * screen.js
  */
 ;(() => {
-  return
   /** @type {HTMLDivElement | null} */
-  const startScreen = document.querySelector('.js-b-start')
+  const startScreen = document.querySelector('.js-start')
   /** @type {HTMLDivElement | null} */
-  const firstScreen = document.querySelector('.js-b-first')
+  const setScreen = document.querySelector('.js-set')
+  /** @type {HTMLButtonElement | null} */
+  const startBtn = document.querySelector('.js-start-btn')
 
-  /** @type {HTMLButtonElement | null} */
-  const startBtn = document.querySelector('.js-b-start-btn')
-  /** @type {HTMLButtonElement | null} */
-  const screenNextBtn = document.querySelector('.js-screen-next-btn')
-  /** @type {HTMLButtonElement | null} */
-  const screenPrevBtn = document.querySelector('.js-screen-prev-btn')
-
-  if (
-    !startScreen ||
-    !firstScreen ||
-    !startBtn ||
-    !screenNextBtn ||
-    !screenPrevBtn
-  ) {
-    console.error(
-      'startScreen, firstScreen, startBtn, screenNextBtn, screenPrevBtn does not exist'
-    )
+  if (!startScreen || !setScreen || !startBtn) {
+    console.error('startScreen, setScreen, startBtn does not exist')
     return
   }
 
-  let index = 0
-  const screens = [startScreen, firstScreen]
-
-  function nextScreen() {
-    screens[index].setAttribute('hidden', 'hidden')
-    index = screens.length - 1 !== index ? index + 1 : 0
-
-    screens[index].removeAttribute('hidden')
-  }
-
-  function prevScreen() {
-    screens[index].setAttribute('hidden', 'hidden')
-    index = index === 0 ? screens.length - 1 : index - 1
-
-    screens[index].removeAttribute('hidden')
-  }
-
-  startBtn.addEventListener('click', nextScreen)
-  screenPrevBtn.addEventListener('click', prevScreen)
+  startBtn.addEventListener('click', () => {
+    startScreen.setAttribute('hidden', 'hidden')
+    setScreen.removeAttribute('hidden')
+  })
 })()
