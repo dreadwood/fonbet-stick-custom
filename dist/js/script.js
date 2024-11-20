22b9722677af1694bdf12a0b854a91e78cdad957
srg <skabassjob@gmail.com>
2024-11-20 17:33:53 +0400

    build: update

--- dist/js/script.js
+++ dist/js/script.js
@@ -103,18 +103,18 @@ window.utils = {
  */
 ;(() => {
   // auth start
-  var regFlag = false
+  let regFlag = false
 
-  var refreshIntervalId = setInterval(function () {
+  let refreshIntervalId = setInterval(() => {
     if (typeof window.registrationApi !== 'undefined') regFlag = true
     if (regFlag) {
       clearInterval(refreshIntervalId)
       document.dispatchEvent(new Event('registrationInit'))
       // console.log('window.registrationApi', window.registrationApi)
-      registrationApi.onRegistrationStateChanged = function (state) {
+      registrationApi.onRegistrationStateChanged = (state) => {
         console.log('registrationApi state', state)
       }
-      registrationApi.onRegistrationCompleted = function (clientId) {
+      registrationApi.onRegistrationCompleted = (clientId) => {
         document.dispatchEvent(
           new CustomEvent('registrationCompleted', {
             detail: { clientId: clientId }
@@ -195,189 +195,67 @@ window.utils = {
 })()
 
 /**
- * form.js
+ * modal.js
  */
 ;(() => {
-  //
-  // const form = container.querySelector('.js-auth-form')
-  // const telEl = container.querySelector('.js-auth-el-tel')
-  // const bdayEl = container.querySelector('.js-auth-el-bday')
-  // const passEl = container.querySelector('.js-auth-el-pass')
-  // const agreeEl = container.querySelector('.js-auth-el-agree')
-  // const ad = container.querySelector('.js-auth-checkbox-ad')
-  // const TEL_LENGTH = 11
-  // const MASK_TEL = '+{7} ({9}00) 000-00-00'
-  // const MIN_AGE_BDAY = 18
-  // function initTelField(fieldWrp, mask, telLength) {
-  //   const field = fieldWrp.querySelector('input[type="tel"]')
-  //   const error = fieldWrp.querySelector('[data-field-error]')
-  //   /**
-  //    * IMask 7.6.1
-  //    * vendor/imask.js
-  //    */
-  //   const telMask = IMask(field, { mask })
-  //   field.addEventListener('input', () => {
-  //     const isComplete = telMask.unmaskedValue.length === telLength
-  //     if (isComplete) {
-  //       fieldWrp.classList.remove('error')
-  //     }
-  //   })
-  //   return [
-  //     field,
-  //     () => {
-  //       const isError = telMask.unmaskedValue.length !== telLength
-  //       if (isError) {
-  //         telEl.classList.add('error')
-  //       }
-  //       return !isError
-  //     }
-  //   ]
-  // }
-  // function initBirthdayField(fieldWrp, minAge) {
-  //   const BIRTHDAY_LENGTH = 10
-  //   const field = fieldWrp.querySelector('input[type="text"]')
-  //   const error = fieldWrp.querySelector('[data-field-error]')
-  //   const date18YearsAgo = new Date()
-  //   date18YearsAgo.setFullYear(new Date().getFullYear() - minAge)
-  //   /**
-  //    * IMask 7.6.1
-  //    * vendor/imask.js
-  //    */
-  //   const birthdayMask = IMask(field, {
-  //     mask: Date,
-  //     min: new Date(1900, 0, 1),
-  //     max: date18YearsAgo,
-  //     lazy: false
-  //   })
-  //   field.addEventListener('input', () => {
-  //     const isComplete = birthdayMask.unmaskedValue.length === BIRTHDAY_LENGTH
-  //     if (isComplete) {
-  //       fieldWrp.classList.remove('error')
-  //     }
-  //   })
-  //   return [
-  //     field,
-  //     () => {
-  //       const isError = birthdayMask.unmaskedValue.length !== BIRTHDAY_LENGTH
-  //       if (isError) {
-  //         fieldWrp.classList.add('error')
-  //         error.textContent = `Вам должно быть ${minAge} лет`
-  //       }
-  //       return !isError
-  //     }
-  //   ]
-  // }
-  // function initPassField(fieldWrp) {
-  //   const field = fieldWrp.querySelector('input[type="password"]')
-  //   const btn = fieldWrp.querySelector('button[data-field-show]')
-  //   const error = fieldWrp.querySelector('[data-field-error]')
-  //   const checkValidPass = () => {
-  //     let isError = false
-  //     const regexMinLatin = /[A-Za-z]/
-  //     const regexMinNum = /\d/
-  //     const regexOnlyLatinNum = /^[A-Za-z\d]+$/
-  //     switch (true) {
-  //       case !regexMinLatin.test(field.value):
-  //         isError = true
-  //         error.textContent = 'Пароль должен содержать латинские буквы'
-  //         break
-  //       case !regexMinNum.test(field.value):
-  //         isError = true
-  //         error.textContent = 'Пароль должен содержать цифры'
-  //         break
-  //       case !regexOnlyLatinNum.test(field.value):
-  //         isError = true
-  //         error.textContent =
-  //           'Пароль может содержать только цифры и латинские буквы'
-  //         break
-  //       case field.value.length < 8:
-  //         isError = true
-  //         error.textContent = 'Пароль должен быть не менее 8 символов'
-  //         break
-  //       default:
-  //         error.textContent = ''
-  //         break
-  //     }
-  //     return isError
-  //   }
-  //   field.addEventListener('input', () => {
-  //     if (field.value.length > 0) {
-  //       btn.removeAttribute('hidden')
-  //     } else {
-  //       btn.setAttribute('hidden', 'hidden')
-  //     }
-  //     const isError = checkValidPass()
-  //     if (!isError) {
-  //       fieldWrp.classList.remove('error')
-  //     }
-  //   })
-  //   btn.addEventListener('click', () => {
-  //     if (field.type === 'password') {
-  //       field.type = 'text'
-  //       btn.classList.add('show')
-  //     } else {
-  //       console.log(field.type)
-  //       field.type = 'password'
-  //       btn.classList.remove('show')
-  //     }
-  //   })
-  //   return [
-  //     field,
-  //     () => {
-  //       const isError = checkValidPass()
-  //       if (isError) {
-  //         fieldWrp.classList.add('error')
-  //       } else {
-  //         fieldWrp.classList.remove('error')
-  //       }
-  //       return !isError
-  //     }
-  //   ]
-  // }
-  // function initCheckbox(fieldWrp) {
-  //   const field = fieldWrp.querySelector('input[type="checkbox"]')
-  //   if (field.checked) {
-  //     fieldWrp.classList.add('error')
-  //   }
-  //   field.addEventListener('change', () => {
-  //     if (field.checked) {
-  //       fieldWrp.classList.remove('error')
-  //     } else {
-  //       fieldWrp.classList.add('error')
-  //     }
-  //   })
-  //   return [
-  //     field,
-  //     () => {
-  //       if (!field.checked) {
-  //         fieldWrp.classList.add('error')
-  //       }
-  //       return field.checked
-  //     }
-  //   ]
-  // }
-  // const [telField, checkValidTel] = initTelField(telEl, MASK_TEL, TEL_LENGTH)
-  // const [bdayField, checkValidBday] = initBirthdayField(bdayEl, MIN_AGE_BDAY)
-  // const [passField, checkValidPass] = initPassField(passEl)
-  // const [agree, checkAgree] = initCheckbox(agreeEl)
-  // form.addEventListener('submit', (evt) => {
-  //   evt.preventDefault()
-  //   const isValidTel = checkValidTel()
-  //   const isValidBday = checkValidBday()
-  //   const isValidPass = checkValidPass()
-  //   const isValidAgree = checkAgree()
-  //   if ((!isValidTel, !isValidBday, !isValidPass, !isValidAgree)) {
-  //     return
-  //   }
-  //   const fields = {
-  //     [telField.name]: telField.value,
-  //     [bdayField.name]: bdayField.value,
-  //     [passField.name]: passField.value,
-  //     [agree.name]: agree.checked,
-  //     [ad.name]: ad.checked
-  //   }
-  //   console.log(fields)
-  // })
+  window.modal = {
+    /**
+     * @param {HTMLDivElement} modalBet
+     * @param {HTMLDivElement} modalShare
+     */
+    initBet(modalBet, modalShare) {
+      /** @type {HTMLLinkElement | null} */
+      const modalBetBtn = modalBet.querySelector('.js-modal-bet-btn')
+
+      modalBetBtn.addEventListener('click', () => {
+        window.utils.closeModal(modalBet)
+        window.utils.showModal(modalShare)
+      })
+    },
+
+    /**
+     * @param {HTMLDivElement} modalShare
+     * @param {HTMLDivElement} modalScore
+     */
+    initShare(modalShare, modalScore) {
+      /** @type {HTMLLinkElement | null} */
+      const btnShare = modalShare.querySelector('.js-modal-share-btn-share')
+      /** @type {HTMLLinkElement | null} */
+      const btnSite = modalShare.querySelector('.js-modal-share-btn-site')
+
+      const openNextModal = () => {
+        window.utils.closeModal(modalShare)
+        window.utils.showModal(modalScore)
+      }
+
+      btnShare.addEventListener('click', () => openNextModal())
+      btnSite.addEventListener('click', () => openNextModal())
+
+      modalShare.addEventListener('click', (evt) => {
+        if (evt.target === modalShare) {
+          openNextModal()
+        }
+      })
+    },
+
+    /**
+     * @param {HTMLDivElement} modalScore
+     */
+    initScore(modalScore) {
+      /** @type {HTMLButtonElement | null} */
+      const btnClose = modalScore.querySelector('.js-score-btn-close')
+
+      btnClose.addEventListener('click', () =>
+        window.utils.closeModal(modalScore)
+      )
+
+      modalScore.addEventListener('click', (evt) => {
+        if (evt.target === modalScore) {
+          window.utils.closeModal(modalScore)
+        }
+      })
+    }
+  }
 })()
 
 /**
@@ -435,11 +313,11 @@ window.utils = {
   /** @type {HTMLDivElement | null} */
   const resultScreen = document.querySelector('.js-result-end')
   /** @type {HTMLDivElement | null} */
-  const modalAuth = document.querySelector('.js-auth-overlay')
+  const modalAuth = document.querySelector('.js-modal-auth')
   /** @type {HTMLButtonElement | null} */
   const startBtn = document.querySelector('.js-start-btn')
   /** @type {HTMLButtonElement | null} */
-  const closeBtn = document.querySelector('.js-auth-close')
+  const modalAuthCloseBtn = modalAuth.querySelector('.js-modal-auth-btn-close')
 
   if (
     !loadingScreen ||
@@ -447,7 +325,7 @@ window.utils = {
     !setScreen ||
     !startBtn ||
     !modalAuth ||
-    !closeBtn
+    !modalAuthCloseBtn
   ) {
     return
   }
@@ -468,14 +346,12 @@ window.utils = {
   }
 
   const openModal = () => {
-    modalAuth.classList.add('show')
-    document.body.classList.add('scroll-lock')
+    window.utils.showModal(modalAuth)
     document.addEventListener('keydown', onEscKeydown)
   }
 
   const closeModal = () => {
-    modalAuth.classList.remove('show')
-    document.body.classList.remove('scroll-lock')
+    window.utils.closeModal(modalAuth)
     document.removeEventListener('keydown', onEscKeydown)
   }
 
@@ -513,18 +389,18 @@ window.utils = {
     }
   }
 
-  document.addEventListener('userInfoUpdated', (event) => {
-    console.log('User info updated:', event.detail)
-    console.log('Previous Client ID:', event.detail.prevClientId)
-    console.log('Current Client ID:', event.detail.clientId)
-    if (event.detail.clientId !== event.detail.prevClientId) {
+  document.addEventListener('userInfoUpdated', (evt) => {
+    console.log('User info updated:', evt.detail)
+    console.log('Previous Client ID:', evt.detail.prevClientId)
+    console.log('Current Client ID:', evt.detail.clientId)
+    if (evt.detail.clientId !== evt.detail.prevClientId) {
       closeModal()
       openSet()
     }
   })
 
-  document.addEventListener('registrationCompleted', async (event) => {
-    const clientId = event.detail.clientId
+  document.addEventListener('registrationCompleted', async (evt) => {
+    const clientId = evt.detail.clientId
 
     if (clientId) {
       window.utils.clientId = clientId
@@ -563,7 +439,7 @@ window.utils = {
     }
   })
 
-  closeBtn.addEventListener('click', () => closeModal())
+  modalAuthCloseBtn.addEventListener('click', () => closeModal())
 
   modalAuth.addEventListener('click', (evt) => {
     if (evt.target === modalAuth) closeModal()
@@ -588,7 +464,6 @@ window.utils = {
     stickBladeTexture: 0,
     stickName: '',
     stickNameColor: 'black',
-
     stickFormGrip: 'right',
     stickFormFlex: '90',
 
@@ -636,9 +511,9 @@ window.utils = {
       this.initWindowFourth()
       this.initWindowFifth()
 
-      this.initModalBet()
-      this.initModalScore()
-      this.initModalShare()
+      window.modal.initBet(this.modalBet, this.modalShare)
+      window.modal.initShare(this.modalShare, this.modalScore)
+      window.modal.initScore(this.modalScore)
     },
 
     prevStep() {
@@ -916,50 +791,6 @@ window.utils = {
       form.addEventListener('change', () => {
         this.stickFormGrip = form.elements.grip.value
         this.stickFormFlex = +form.elements.flex.value
-      })
-    },
-
-    initModalBet() {
-      const modalBetBtn = this.modalBet.querySelector('.js-modal-bet-btn')
-
-      modalBetBtn.addEventListener('click', () => {
-        window.utils.closeModal(this.modalBet)
-        window.utils.showModal(this.modalShare)
-      })
-    },
-
-    initModalShare() {
-      const btnShare = this.modalShare.querySelector(
-        '.js-modal-share-btn-share'
-      )
-      const btnSite = this.modalShare.querySelector('.js-modal-share-btn-site')
-
-      const openNextModal = () => {
-        window.utils.closeModal(this.modalShare)
-        window.utils.showModal(this.modalScore)
-      }
-
-      btnShare.addEventListener('click', () => openNextModal())
-      btnSite.addEventListener('click', () => openNextModal())
-
-      this.modalShare.addEventListener('click', (evt) => {
-        if (evt.target === this.modalShare) {
-          openNextModal()
-        }
-      })
-    },
-
-    initModalScore() {
-      const btnClose = this.modalScore.querySelector('.js-score-btn-close')
-
-      btnClose.addEventListener('click', () =>
-        window.utils.closeModal(this.modalScore)
-      )
-
-      this.modalScore.addEventListener('click', (evt) => {
-        if (evt.target === this.modalScore) {
-          window.utils.closeModal(this.modalScore)
-        }
       })
     },
 
